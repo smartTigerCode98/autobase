0414d9c62ae5d6517acc67376298b91ec08a76c4
Yuriy Danko <danko98uran@gmail.com>
2019-05-31 10:55:23 +0300

    removed service from the signature of the constructor

--- Desktop/documents/KPI/3 курс/2 семестр/components/autobase/src/main/java/command/Command.java
+++ Desktop/documents/KPI/3 курс/2 семестр/components/autobase/src/main/java/command/Command.java
@@ -1,12 +1,8 @@
 package command;
 
-import service.AutoBaseService;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public interface Command <TResult>{
-    public TResult execute(HttpServletRequest request, HttpServletResponse response, AutoBaseService service);
+    TResult execute(HttpServletRequest request, HttpServletResponse response);
 }
